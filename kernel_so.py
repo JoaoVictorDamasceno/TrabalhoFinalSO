8f94b81671ad945b25ad14559effa771f7051aa2
Yan Pedro <yanpedrobrasileiro@gmail.com>
2026-06-23 18:54:53 -0300

    Implementação do gerenciador de recursos

--- kernel_so.py
+++ kernel_so.py
@@ -1,8 +1,5 @@
-Python
 import threading
-import time
 import logging
-
 
 logging.basicConfig(level=logging.INFO, format='[%(relativeCreated)05d ms] [%(levelname)s]%(message)s')
 
@@ -13,3 +10,18 @@ class SistemaACL:
     def validar(self, usuario, arquivo, modo):
         permissoes = self.regras.get(arquivo, {}).get(usuario, [])
         return modo in permissoes
+    
+class GerenciadorDeRecursos:
+    def __init__(self, acl):
+        self.acl = acl
+        self.locks = {}
+        self.alocados = {}
+        self.esperando = {}
+        self.lock_so = threading.Lock()
+        self.metricas = {"acessos_negados": 0, "deadlocks_resolvidos": 0, "tempo_espera_total": 0}
+
+    def registrar_processo(self, nome_processo):
+        with self.lock_so:
+            if nome_processo not in self.alocados:
+                self.alocados[nome_processo] = set()
+                self.esperando[nome_processo] = None
