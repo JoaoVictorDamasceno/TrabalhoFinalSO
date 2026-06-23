b6f183d7a5af509331fa2ac170abae3d2b2c434b
Ricardo Magalhães Parente Frota <ricardoparente58@gmail.com>
2026-06-23 19:05:31 -0300

    Implementado o método solicitar_bloqueio integrando a verificação de ACL e a espera pelo Mutex do arquivo e Adicionados métodos para liberar arquivos individualmente ou forçar a limpeza total de um processo.

--- kernel_so.py
+++ kernel_so.py
@@ -25,3 +25,51 @@ class GerenciadorDeRecursos:
             if nome_processo not in self.alocados:
                 self.alocados[nome_processo] = set()
                 self.esperando[nome_processo] = None
+                
+    def solicitar_bloqueio(self, processo, usuario, arquivo, modo):
+        with self.lock_so:
+            self.metricas["total_acessos"] += 1
+            if processo not in self.esperando: self.esperando[processo] = None
+            
+        if not self.acl.validar(usuario, arquivo, modo):
+            logging.warning(f"ACL NEGOU: '{processo}' ({usuario}) -> '{arquivo}'")
+            with self.lock_so:
+                self.metricas["negados"] += 1
+                self.metricas["falhos"] += 1
+            return False
+            
+        with self.lock_so: self.metricas["permitidos"] += 1
+
+        with self.lock_so:
+            if arquivo not in self.locks: self.locks[arquivo] = threading.Lock()
+            self.esperando[processo] = arquivo
+
+        inicio_espera = time.time()
+        adquirido = False
+        while not adquirido:
+            adquirido = self.locks[arquivo].acquire(timeout=0.2)
+            with self.lock_so:
+                if processo not in self.alocados and processo not in self.esperando:
+                    return False # Abortado pelo Monitor
+
+        espera = time.time() - inicio_espera
+        with self.lock_so:
+            self.metricas["espera_total"] += espera
+            self.esperando[processo] = None
+            self.alocados[processo].add(arquivo)
+            
+        logging.info(f"LOCK ADQUIRIDO: '{processo}' -> '{arquivo}' ({espera:.3f}s)")
+        return True
+
+    def liberar_arquivo(self, processo, arquivo):
+        with self.lock_so:
+            if arquivo in self.alocados.get(processo, set()):
+                self.locks[arquivo].release()
+                self.alocados[processo].remove(arquivo)
+
+    def liberar_todos(self, processo):
+        with self.lock_so:
+            for arq in list(self.alocados.get(processo, set())):
+                self.locks[arq].release()
+            self.alocados.pop(processo, None)
+            self.esperando.pop(processo, None)
